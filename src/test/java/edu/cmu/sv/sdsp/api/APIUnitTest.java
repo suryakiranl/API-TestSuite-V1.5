635061cb6bff7504823cbadf19d97d377b16a6b4
Surya Kiran <suryakiranl@gmail.com>
2014-03-27 06:21:27 -0700

    Added test case to test getAllDevices

--- src/test/java/edu/cmu/sv/sdsp/api/APIUnitTest.java
+++ src/test/java/edu/cmu/sv/sdsp/api/APIUnitTest.java
@@ -1,12 +1,41 @@
 package edu.cmu.sv.sdsp.api;
 
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import edu.cmu.sv.sdsp.api.helper.APIHelper;
+import edu.cmu.sv.sdsp.utils.Constants;
+import edu.cmu.sv.sdsp.utils.Logger;
+
 /**
  * Class to perform individual unit testing on API to check if the API
  * operations work independently.
  * 
  * @author Surya Kiran
- *
  */
-public class APIUnitTest {
-
+@RunWith(JUnit4.class)
+public class APIUnitTest extends BaseTest {
+	private static final Logger log = Logger.get();
+	/**
+	 *  Add a rule that all JUnit test cases run for a maximum of 2 minutes.
+	 */
+	@Rule
+	public Timeout testCaseTimeOut = new Timeout(Constants.JUNIT_TIMEOUT);
+	
+	@Test
+	public void testGetAllDevices() {
+		try {
+			String resp = APIHelper.processGetAllDevices(null);
+			assertReponseNotNull(resp);
+		} catch (IOException e) {
+			log.error(e);
+			Assert.fail();
+		}
+	}
 }
