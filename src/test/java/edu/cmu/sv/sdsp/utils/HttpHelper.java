9d873465e8511a2e94e04500846478efcb256153
Surya Kiran <suryakiranl@gmail.com>
2014-03-27 15:43:43 -0700

    Added methods to perform POST and DELETE RESTful operations

--- src/test/java/edu/cmu/sv/sdsp/utils/HttpHelper.java
+++ src/test/java/edu/cmu/sv/sdsp/utils/HttpHelper.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 
 /**
  * Utility Helper class to make GET and POST requests to a URL. This class also
@@ -32,8 +35,7 @@ public class HttpHelper {
 	public static String performHttpGet(String urlStr) throws HttpException, IOException {
 		log.enter(urlStr);
 		
-		HttpClient client = new HttpClient();
-		client.getHttpConnectionManager().getParams().setConnectionTimeout(Constants.CONNECTION_TIMEOUT);
+		HttpClient client = getHttpClient();
 				
 		GetMethod get = new GetMethod(urlStr);
 		get.setFollowRedirects(true);
@@ -47,12 +49,66 @@ public class HttpHelper {
 			get.releaseConnection();
 		}
 		
+		logResponse(response);
+		
+		return response;
+	}
+	
+	public static String performHttpPost(String urlStr, String content) throws HttpException, IOException {
+		log.enter(urlStr, content);
+		
+		String response = null;
+		HttpClient client = getHttpClient();
+
+		PostMethod post = new PostMethod(urlStr);
+		StringRequestEntity sre = new StringRequestEntity(content, Constants.CONTENT_TYPE_JSON, Constants.CHARSET_UTF8);
+		post.setRequestEntity(sre);
+		
+		try {
+			int respCode = client.executeMethod(post);
+			log.debug("Response code from server :: " + respCode);
+			response = post.getResponseBodyAsString();
+		} finally {
+			post.releaseConnection();
+		}
+		
+		logResponse(response);
+		return response;
+	}
+	
+	public static String performHttpDelete(String urlStr) throws HttpException, IOException {
+		log.enter(urlStr);
+		
+		String response = null;
+		HttpClient client = getHttpClient();
+		
+		DeleteMethod delete = new DeleteMethod(urlStr);
+		delete.setFollowRedirects(true);
+		
+		try {
+			int responseCode = client.executeMethod(delete);
+			log.debug("Response code from server :: " + responseCode);
+			response = delete.getResponseBodyAsString();	
+		} finally {
+			delete.releaseConnection();
+		}
+		
+		logResponse(response);
+		return response;
+	}
+	
+	private static HttpClient getHttpClient() {
+		HttpClient client = new HttpClient();
+		client.getHttpConnectionManager().getParams().setConnectionTimeout(Constants.CONNECTION_TIMEOUT);
+		
+		return client;
+	}
+	
+	private static void logResponse(String response) {
 		if(response == null || response.length() <= 200) {
 			log.exit(response);
 		} else {
 			log.exit(response.substring(0, 199) + " << Truncated");
 		}
-		
-		return response;
 	}
 }
