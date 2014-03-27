13736661850127028edbe6bfc899fd5f82f3cda1
Surya Kiran <suryakiranl@gmail.com>
2014-03-27 05:01:15 -0700

    API Unit test file moved to new package

---
+++ src/test/java/edu/cmu/sv/sdsp/api/APIUnitTest.java
@@ -0,0 +1,12 @@
+package edu.cmu.sv.sdsp.api;
+
+/**
+ * Class to perform individual unit testing on API to check if the API
+ * operations work independently.
+ * 
+ * @author Surya Kiran
+ *
+ */
+public class APIUnitTest {
+
+}
