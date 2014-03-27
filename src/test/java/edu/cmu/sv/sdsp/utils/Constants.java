c1f8031895170ed96a6310eead3ba2636db2343e
Surya Kiran <suryakiranl@gmail.com>
2014-03-27 04:58:42 -0700

    File with list of constants used in this project

---
+++ src/test/java/edu/cmu/sv/sdsp/utils/Constants.java
@@ -0,0 +1,15 @@
+package edu.cmu.sv.sdsp.utils;
+
+/**
+ * All the constants used in this project are listed here.
+ * It will be easy to externalize these values when we 
+ * do internationalization, or want to load paramter values 
+ * from external properties file.
+ * 
+ * @author Surya Kiran
+ *
+ */
+public class Constants {
+	// Time out for any HTTP connections
+	public static final int CONNECTION_TIMEOUT = 30000;
+}
