d9a76bf0ea646b7832944e0fb9d2da38b117b43a
Surya Kiran <suryakiranl@gmail.com>
2014-03-30 08:14:17 -0700

    Refactor clean up methods into finally block in getAllXXX Tests

--- src/test/java/edu/cmu/sv/sdsp/api/APIUnitTest.java
+++ src/test/java/edu/cmu/sv/sdsp/api/APIUnitTest.java
@@ -2,14 +2,12 @@ package edu.cmu.sv.sdsp.api;
 
 import java.io.IOException;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import edu.cmu.sv.sdsp.api.helper.APIHelper;
 import edu.cmu.sv.sdsp.api.helper.APIHelper.ResultType;
-import edu.cmu.sv.sdsp.utils.Logger;
 
 /**
  * Class to perform individual unit testing on API to check if the API
@@ -19,170 +17,153 @@ import edu.cmu.sv.sdsp.utils.Logger;
  */
 @RunWith(JUnit4.class)
 public class APIUnitTest extends BaseTest {
-	private static final Logger log = Logger.get();
-	
 	@Test
 	public void testGetAllDevices() {
 		try {
 			String resp = APIHelper.getAllDevices(null);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllDevicesCSV() {
 		try {
 			String resp = APIHelper.getAllDevices(ResultType.CSV);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllDevicesJSON() {
 		try {
 			String resp = APIHelper.getAllDevices(ResultType.JSON);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllDeviceTypes() {
 		try {
 			String resp = APIHelper.getAllDeviceTypes(null);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllDeviceTypesCSV() {
 		try {
 			String resp = APIHelper.getAllDeviceTypes(ResultType.CSV);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllDeviceTypesJSON() {
 		try {
 			String resp = APIHelper.getAllDeviceTypes(ResultType.JSON);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorTypes() {
 		try {
 			String resp = APIHelper.getAllSensorTypes(null);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorTypesCSV() {
 		try {
 			String resp = APIHelper.getAllSensorTypes(ResultType.CSV);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorTypesJSON() {
 		try {
 			String resp = APIHelper.getAllSensorTypes(ResultType.JSON);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensors() {
 		try {
 			String resp = APIHelper.getAllSensors(null);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorsCSV() {
 		try {
 			String resp = APIHelper.getAllSensors(ResultType.CSV);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorsJSON() {
 		try {
 			String resp = APIHelper.getAllSensors(ResultType.JSON);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorCategories() {
 		try {
 			String resp = APIHelper.getAllSensorCategories(null);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorCategoriesCSV() {
 		try {
 			String resp = APIHelper.getAllSensorCategories(ResultType.CSV);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
-	
+
 	@Test
 	public void testGetAllSensorCategoriesJSON() {
 		try {
 			String resp = APIHelper.getAllSensorCategories(ResultType.JSON);
 			assertReponseNotNull(resp);
 		} catch (IOException e) {
-			log.error(e);
-			Assert.fail();
+			processException(e);
 		}
 	}
 }
